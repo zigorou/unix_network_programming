d9808b05dafcd89f74a4a86a7c2e6926855a9fef
Toru Yamaguchi <zigorou@cpan.org>
2012-01-15 01:01:18 +0900

    fixed op on read buffer. add server code

--- intro/daytimetcpsrv.c
+++ intro/daytimetcpsrv.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <stdarg.h>
 #include <string.h>
 #include <errno.h>
+#include <time.h>
 #include <netinet/in.h>
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <sys/uio.h>
 #include <unistd.h>
-#include <arpa/inet.h>
 
 #define MAXLINE 1000
+#define LISTENQ 100
 
 void err_doit(int useerrno, const char *fmt, va_list ap) {
     int errno_orig;
@@ -45,33 +46,35 @@ void err_sys(const char *fmt, ...) {
 }
 
 int main(int argc, char **argv) {
-    int sockfd, n;
-    char recvline[MAXLINE - 1];
+    int listenfd, connfd;
     struct sockaddr_in servaddr;
+    char buff[MAXLINE];
+    time_t ticks;
 
-    if (argc != 2)
-        err_quit("Usage: %s <ipaddress>", argv[0]);
-
-    if ( (sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0 )
-        err_sys("Socket error");
+    // fprintf(stderr, "socket");
+    if ( ( listenfd = socket(AF_INET, SOCK_STREAM, 0) ) < 0 )
+        err_sys("socket error");
 
     servaddr.sin_family = AF_INET;
-    servaddr.sin_port   = htons(13);
-   
-    if ( inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0)
-        err_quit("inet_pton error for %s", argv[1]);
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servaddr.sin_port = htons(13);
 
-    if ( connect(sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr)) < 0 )
-        err_sys("Connect error");
-    
-    while ( (n = read(sockfd, recvline, MAXLINE)) > 0 ) {
-        recvline[n] = '\0';
-        if ( fputs(recvline, stdout) == EOF )
-            err_sys("fputs error");
+    if ( bind(listenfd, (struct sockaddr *) &servaddr, sizeof(servaddr)) < 0 )
+        err_sys("bind error");
+
+    if ( listen(listenfd, LISTENQ) < 0 )
+        err_sys("listen error");
+
+    for (;;) {
+        if ( ( connfd = accept(listenfd, (struct sockaddr *) NULL, NULL) ) < 0 )
+            err_sys("accept error");
+
+        ticks = time(NULL);
+        snprintf(buff, sizeof(buff), "%.24s\r\n", ctime(&ticks));
+
+        if ( write(connfd, buff, strlen(buff)) < 0 )
+            err_sys("write error");
+
+        close(connfd);
     }
-
-    if ( n < 0 )
-        err_sys("read error\n");
-
-    exit(0);
 }
