2ddcc7498df43a2112b96caaed2d31be4fd74402
Toru Yamaguchi <zigorou@cpan.org>
2012-01-15 21:54:38 +0900

    Modulized

--- intro/daytimetcpsrv.c
+++ intro/daytimetcpsrv.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdarg.h>
-#include <string.h>
 #include <errno.h>
 #include <time.h>
 #include <netinet/in.h>
@@ -10,40 +8,10 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
+#include "error.h"
+
 #define MAXLINE 1000
 #define LISTENQ 100
-
-void err_doit(int useerrno, const char *fmt, va_list ap) {
-    int errno_orig;
-    char buf[MAXLINE + 1];
-
-    errno_orig = errno;
-    vsnprintf(buf, MAXLINE, fmt, ap);
-    if (useerrno) {
-        strcat(buf, ": ");
-        strcat(buf, strerror(errno_orig));
-    }
-    strcat(buf, "\n");
-    fputs(buf, stderr);
-
-    return;
-}
-
-void err_quit(const char *fmt, ...) {
-    va_list ap;
-    va_start(ap, fmt);
-    err_doit(0, fmt, ap);
-    va_end(ap);
-    exit(1);
-}
-
-void err_sys(const char *fmt, ...) {
-    va_list ap;
-    va_start(ap, fmt);
-    err_doit(1, fmt, ap);
-    va_end(ap);
-    exit(1);
-}
 
 int main(int argc, char **argv) {
     int listenfd, connfd;
